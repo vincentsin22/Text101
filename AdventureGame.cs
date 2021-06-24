0f934dfb6764f45426373510d8f580940e7e32b6
Vincent <45884633+vincentsin22@users.noreply.github.com>
2021-06-24 15:46:46 -0400

    End of Chapter 21
    Created the script for the textComponent in the UnityEngine

---
+++ AdventureGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //this was added so we can use the variable Text in line 9. This is because Text is a part of the UI and it needs to be identified. 
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent; //[serializeField] is used in front of variables so we can make this available in the Unity Inspector. If we check, textComponent will be available. 
+                     //Text is a type of variable. textComponent is the variable name (I Think)
+
+    // Start is called before the first frame update
+    void Start()
+    { //the dot after textComponent means it will access the text within it 
+        textComponent.text = ("I am added programmatically");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
